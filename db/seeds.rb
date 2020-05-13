98330041b0efc542f6bbb374d7991d7a502fd555
Daniel Howitt <daniel.howittcontact@gmail.com>
2020-05-13 15:41:41 +0100

    Yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: 'french' }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: 'italian' }
+blacklock = { name: "Blacklock", address: "28-30 Rivington St, London EC2A 3DZ", category: 'french' }
+machiya =  { name: "Machiya", address: "5 Panton St, London SW1Y 4DL", category: 'japanese' }
+masgouf =  { name: "Masgouf", address: "134 Cromwell Rd, London SW7 4HA", category: 'belgian' }
+
+[ dishoom, pizza_east, blacklock, machiya, masgouf ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
